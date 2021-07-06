ce9740dc1341bf17455c470f945894f5c0ac0e43
Badjiojan <dmitriy_demyanchenko@mail.ru>
2021-07-06 21:09:37 +0300

    dhjd

--- OOP обстр классы/Header.h
+++ OOP обстр классы/Header.h
@@ -1,1 +1,48 @@
 #pragma once
+#include <iostream>
+#include "Windows.h"
+using namespace std;
+
+class Weapon
+{
+	/*int count;*/
+public:
+	void virtual OneUse() = 0;
+	void virtual Attact(int count)
+	{
+		for (int i = 0; i < count; i++)
+		{
+			this->OneUse();
+			Sleep(50);
+		}
+	}
+	virtual ~Weapon(){}
+};
+
+class Gun :virtual public Weapon
+{
+public:
+	void OneUse()override
+	{
+		cout << "Gun ----->" << endl;
+	}
+
+};
+class Ñrossbow :virtual public Weapon
+{
+public:
+	void OneUse()override
+	{
+		cout << "Crossbow ----->" << endl;
+	}
+
+};
+class Bomb :virtual public Weapon
+{
+public:
+	void OneUse()override
+	{
+		cout << "Bomb ----->" << endl;
+	}
+
+};
--- OOP обстр классы/OOP обстр классы.cpp
+++ OOP обстр классы/OOP обстр классы.cpp
@@ -2,25 +2,45 @@
 
 #include <iostream>
 #include "Employer.h"
+#include "Header.h"
 
 int main()
 {
-	Employer** employer = new Employer * [3];
-	employer[0] = new President;
-	employer[1] = new Manager;
-	employer[2] = new Worker;
+	//Employer** employer = new Employer * [3];
+	//employer[0] = new President;
+	//employer[1] = new Manager;
+	//employer[2] = new Worker;
+
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	employer[i]->Print();
+	//	cout << endl;
+	//}
+	////delete employer[0];
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	delete employer[i];
+	//}
+	//delete[] employer;
+
+
+	Weapon** weapon = new Weapon * [3];
+	weapon[0] = new Gun;
+	weapon[1] = new Сrossbow;
+	weapon[2] = new Bomb;
 
 	for (int i = 0; i < 3; i++)
 	{
-		employer[i]->Print();
+		weapon[i]->Attact(5);
 		cout << endl;
 	}
-	//delete employer[0];
+	
 	for (int i = 0; i < 3; i++)
 	{
-		delete employer[i];
+		delete weapon[i];
 	}
-	delete[] employer;
+	delete[] weapon;
+
 }
 
 
